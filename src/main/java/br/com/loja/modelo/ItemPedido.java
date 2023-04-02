a71563b1fe944f8a34f77a284a52b3c2603fae02
Leandro <leandroscarpigranelli@gmail.com>
2023-04-02 11:40:25 -0300

    Criadas tabelas de Cliente, Pedido e ItensPedidos

---
+++ src/main/java/br/com/loja/modelo/ItemPedido.java
@@ -0,0 +1,71 @@
+package br.com.loja.modelo;
+
+import jakarta.persistence.*;
+
+import java.math.BigDecimal;
+
+@Entity
+@Table(name="itens_pedido")
+public class ItemPedido {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private BigDecimal precoUnitario;
+    private Integer quantidade;
+
+    @ManyToOne
+    private Produto produto;
+
+    @ManyToOne
+    private Pedido pedido;
+
+    public ItemPedido(Integer quantidade, Produto produto, Pedido pedido) {
+        this.quantidade = quantidade;
+        this.produto = produto;
+        this.pedido = pedido;
+    }
+
+    public ItemPedido() {
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public BigDecimal getPrecoUnitario() {
+        return precoUnitario;
+    }
+
+    public void setPrecoUnitario(BigDecimal precoUnitario) {
+        this.precoUnitario = precoUnitario;
+    }
+
+    public Integer getQuantidade() {
+        return quantidade;
+    }
+
+    public void setQuantidade(Integer quantidade) {
+        this.quantidade = quantidade;
+    }
+
+    public Produto getProduto() {
+        return produto;
+    }
+
+    public void setProduto(Produto produto) {
+        this.produto = produto;
+    }
+
+    public Pedido getPedido() {
+        return pedido;
+    }
+
+    public void setPedido(Pedido pedido) {
+        this.pedido = pedido;
+    }
+}
